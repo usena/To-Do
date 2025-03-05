e6dca2c9975d48ca9e2af40aa06e87d51553a759
Juwono <unosega@gmail.com>
2025-03-05 15:31:16 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import NavbarComponent from "./components/NavbarComponent";
+import AddTaskComponent from "./components/AddTaskComponent";
+import MyTaskComponent from "./components/MyTaskComponent";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <div className="h-full">
+      <Toaster />
+      {/* navbar component */}
+      <NavbarComponent />
+
+      {/* task container */}
+      <div className="flex w-full h-full gap-8 py-6 px-6">
+        {/* add task component */}
+        <AddTaskComponent />
+
+        {/* my task component */}
+        <div className="flex flex-col gap-3 flex-3/4 bg-green-100 rounded-md p-4">
+          <h1 className="text-green-900 font-semibold text-lg">My Tasks</h1>
+          <MyTaskComponent />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
